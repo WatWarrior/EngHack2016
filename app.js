1e114b715656eb463b7770d83fc7595971add9be
J-Xiao <xiaojijundavid@gmail.com>
2016-10-29 10:30:35 -0400

    Allow change user data.

--- app.js
+++ app.js
@@ -28,7 +28,19 @@ server.post('/api/messages', connector.listen());
 // Bots Dialogs
 //=========================================================
 
-bot.dialog('/', [
+var intents = new builder.IntentDialog();
+bot.dialog('/', intents);
+
+intents.matches(/^change name/i, [
+    function (session) {
+        session.beginDialog('/profile');
+    },
+    function (session, results) {
+        session.send('Ok... Changed your name to %s', session.userData.name);
+    }
+]);
+
+intents.onDefault([
     function (session, args, next) {
         if (!session.userData.name) {
             session.beginDialog('/profile');
