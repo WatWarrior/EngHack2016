6c3b71c8ecff382bd25b656601d72b52296c4717
zzq229 <zhouziqi960620@gmail.com>
2016-10-29 09:13:52 -0400

    change

--- app.js
+++ app.js
@@ -1,21 +1,39 @@
-var restify = require('restify');
+// var restify = require('restify');
 var builder = require('botbuilder');
 
 // Setup Restify Server
-var server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, function () {
-   console.log('%s listening to %s', server.name, server.url); 
-});
+// var server = restify.createServer();
+// server.listen(process.env.port || process.env.PORT || 3978, function () {
+//    console.log('%s listening to %s', server.name, server.url); 
+// });
 
-// Create chat bot
-var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
-});
-var bot = new builder.UniversalBot(connector);
-server.post('/api/messages', connector.listen());
+// // Create chat bot
+// var connector = new builder.ChatConnector({
+//     appId: process.env.MICROSOFT_APP_ID,
+//     appPassword: process.env.MICROSOFT_APP_PASSWORD
+// });
+// var bot = new builder.UniversalBot(connector);
+// server.post('/api/messages', connector.listen());
 
 
-bot.dialog('/', function (session) {
-    session.send("Hello World");
-});
+// bot.dialog('/', function (session) {
+//     session.send("Hello World");
+// });
+
+// var builder = require('botbuilder');
+
+var connector = new builder.ConsoleConnector().listen();
+var bot = new builder.UniversalBot(connector, {
+   localizerSettings: {
+   botLocalePath: "./locale",
+   defaultLocale: "en"
+ }
+ });
+bot.dialog('/', [
+    function (session) {
+        builder.Prompts.text(session, 'Hi! What is your name?');
+    },
+    function (session, results) {
+        session.send('Hello %s!', results.response);
+    }
+]);
